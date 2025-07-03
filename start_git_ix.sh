2dfab74e3cab38eed7935182ee83f2764ed79089
Ryan Miller <ryanlloydmiller@gmail.com>
2025-07-03 14:29:09 -0400

    fix: update start_git_ix.sh to use npm start

--- start_git_ix.sh
+++ start_git_ix.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# /home/lab/git_ix/start_git_ix.sh
+#!/usr/bin/env bash
 SESSION=git_ix
 DIR="$HOME/git_ix"
 
@@ -6,5 +8,5 @@ DIR="$HOME/git_ix"
 if ! tmux has-session -t "$SESSION" 2>/dev/null; then
   cd "$DIR" || exit 1
   # -l ensures bash loads your login env (needed if you rely on nvm, etc.)
-  tmux new-session  -d  -s "$SESSION"  "/bin/bash -lc 'npm start -- -p 3001'"
+  tmux new-session  -d  -s "$SESSION"  "/bin/bash -c 'source \"$HOME/.nvm/nvm.sh\" && npm start -- -p 3001'"
 fi
